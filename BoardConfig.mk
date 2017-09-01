643e8ff0bdbfc12331f49ba2b10ca81239b9b136
WouldChar <wouldchar@gmail.com>
2017-09-01 11:53:55 +0800

    nx510j: Remove selinux and set permissive  * Add HIDL manifest  * Remove seclabels

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,6 @@
 
 DEVICE_PATH := device/nubia/nx510j
 
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -45,7 +43,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 androidboot.selinux=permissive
 TARGET_KERNEL_SOURCE := kernel/nubia/msm8994
 TARGET_KERNEL_CONFIG := lineageos_nx510j_defconfig
 TARGET_USES_64_BIT_BINDER := true
@@ -181,11 +179,6 @@ TARGET_RECOVERY_UPDATER_LIBS := \
     librecovery_updater_msm \
     librecovery_updater_nx510j
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8994 \
