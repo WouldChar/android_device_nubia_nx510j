0c8e31a0cea49a4af5133083efde342606558ded
WouldChar <wouldchar@gmail.com>
2017-11-30 20:20:46 +0800

    Revert "nx510j: Disable HAL3"
    This reverts commit 7d930f474a884f61187907d84076c795b32fa2d9.

--- device.mk
+++ device.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     Snap
 
 # Display HAL
